7a5bfda4054c39de158ef730c4b4cfbf302a90ab
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-09-26 20:10:08 -0300

    criando a função getUserById

---
+++ src/endpoints/users/getUserById.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express"
+import connection from "../../connection"
+import { getTokenData } from "../../services/authenticator"
+import { userTableName } from "../../types"
+
+export async function getUserById(
+    req: Request,
+    res: Response
+): Promise<void> {
+    try {
+
+        const token: string = req.headers.authorization!
+
+        getTokenData(token)
+
+        const [user] = await connection(userTableName)
+        .where({id: req.params.id})
+
+        if (!user) {
+            throw new Error("Usuário não encontrado");
+            
+        }
+
+        const {id, name, email} = user
+
+        res.status(200).send({
+            id,
+            name,
+            email
+        })
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import { getProfile } from "./endpoints/users/getProfile";
+import { getUserById } from "./endpoints/users/getUserById";
 import { login } from "./endpoints/users/login";
 import { signup } from "./endpoints/users/signup";
 import generateId from "./services/idGenerator";
@@ -7,7 +8,7 @@ import generateId from "./services/idGenerator";
 app.post('/users/signup', signup)
 app.post('/users/login', login)
 app.get('/users/profile', getProfile)
-app.get('/users/:id/profile')
+app.get('/users/:id/profile', getUserById)
 
 app.post('/recipe')
 app.get('/recipe/:id')

349770b0fa99fc6215fc25cd9b1840059f151c5e
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-09-26 17:03:21 -0300

    configurando a função authenticator

--- src/services/authenticator.ts
+++ src/services/authenticator.ts
@@ -1,14 +1,27 @@
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { config } from "dotenv";
+import { authenticationData } from "../types";
 
 config()
 
-const {JWT_KEY} = process.env
+const { JWT_KEY } = process.env
 
 export const generateToken = (
     payload: authenticationData
 ): string => sign(
     payload,
     JWT_KEY!,
-    {expiresIn: "1d"}
+    { expiresIn: "1d" }
 )
+
+export const getTokenData = (
+    token: string
+): authenticationData | null => {
+    try {
+        const { id } = verify(token, JWT_KEY!) as authenticationData
+
+        return { id }
+    } catch (error) {
+        return null
+    }
+}

a64c2cb464e7f2952a5fb4775df9dc0fddbcb913
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-09-26 22:33:16 -0300

    criando a função getRecipeById

--- src/endpoints/recipes/getRecipeById.ts
+++ src/endpoints/recipes/getRecipeById.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from "express";
+import connection from "../../connection";
+import { getTokenData } from "../../services/authenticator";
+import { recipeTableName } from "../../types";
 
 export default async function getRecipeById(
     req: Request,
     res: Response
-    ): Promise<void> {
+): Promise<void> {
     try {
-        
+
+        const token = req.headers.authorization!
+
+        getTokenData(token)
+
+        const [recipe] = await connection(recipeTableName)
+            .where({ id: req.params.id })
+
+        res.send({ 
+            id: recipe.id,
+            title: recipe.title,
+            description: recipe.description,
+            createdAt: recipe.created_at
+         })
     } catch (error) {
         console.log(error.message)
         res.status(500).send(error.message)
